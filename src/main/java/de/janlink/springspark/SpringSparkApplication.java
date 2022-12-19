f5802778c1d146fe1e971f44fb01ad6086fb9fa0
janlink <janlink@outlook.com>
2022-12-19 22:55:36 +0100

    initial commit

---
+++ src/main/java/de/janlink/springspark/SpringSparkApplication.java
@@ -0,0 +1,13 @@
+package de.janlink.springspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/janlink/springspark/SpringSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.janlink.springspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
